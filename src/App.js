c27c2c9cbb045ec0553176d62247985491c95cd9
byohannes <42332389+byohannes@users.noreply.github.com>
2020-05-28 17:34:49 +0100

    navbar component setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
-  return <div>Hello</div>;
+  return(
+       <div>
+      <Navbar />
+      </div>) ;
 };
 
 export default App;
