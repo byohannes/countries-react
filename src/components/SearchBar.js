13133dd336da6d876026d1a281e5119f76823017
byohannes <42332389+byohannes@users.noreply.github.com>
2020-06-05 09:02:15 +0100

    search input style updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,17 +3,15 @@ import React from 'react';
 const SearchBar = ({handleInput}) => {
   return (
     <div className="container mb-auto mt-2 ">
-      <div className="input-group-prepend">
-        <span className="input-group-text">
-          <i className="fas fa-search"></i>
-        </span>
+      <div className="form-group has-search">
+        <span className="fa fa-search form-control-feedback"></span>
+        <input
+          type="text"
+          onInput={(e) => handleInput(e)}
+          className="input-control"
+          placeholder="search for a country..."
+        />
       </div>
-      <input
-        type="text" 
-        onInput={(e) => handleInput(e)}
-        className="input-control"
-        placeholder="search for a country..."
-      />
     </div>
   );
 };
